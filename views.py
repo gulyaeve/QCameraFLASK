0554a233e4c0eb3a0b2856cbaea2df6979a2af8c
norby4ever <petrleliaev@gmail.com>
2022-06-14 17:20:41 +0300

    Добавлен ввод порта и интерфейс для второй камеры

--- views.py
+++ views.py
@@ -18,107 +18,85 @@ class UI_Window(QWidget):
         self.timer.timeout.connect(self.nextFrameSlot)
 
         # Create a layout.
-        self.layout = QVBoxLayout()
+        self.layout = QHBoxLayout()
+
+        self.CameraLayout = [QVBoxLayout(), QVBoxLayout()]
 
         self.version = '0.1.1'
 
-        self.isPasswordEntered = False
+        self.isPasswordEntered = [False, False]
         # Add a button
-        self.button_layout = QHBoxLayout()
-        self.password_layout = QHBoxLayout()
+        self.button_layout = [QHBoxLayout(), QHBoxLayout()]
+        self.password_layout = [QHBoxLayout(), QHBoxLayout()]
 
         self.cameras = []
         for i in QCameraInfo.availableCameras():
             self.cameras.append(i.description())
 
-        self.cam1label = QLabel('Камера 1: ')
-        self.camerasCombo = QComboBox()
-        self.camerasCombo.addItems(self.cameras)
-        self.camerasCombo.activated[str].connect(self.onCameraSelect)
-        self.camerasCombo.activated[str].connect(self.start)
-        self.button_layout.addWidget(self.cam1label)
-        self.button_layout.addWidget(self.camerasCombo)
+        self.index = 0
+        self.camlabel = [QLabel('Камера 1: '), QLabel('Камера 2: ')]
+        self.camerasCombo = [QComboBox(), QComboBox()]
+        for i in range(2):
+            self.camerasCombo[i].addItems(self.cameras)
+            #    self.camerasCombo[i].activated[str].connect(self.start)
+        self.camerasCombo[0].activated[str].connect(self.onCamera0Select)
+        self.camerasCombo[1].activated[str].connect(self.onCamera1Select)
+
+        for i in range(2):
+            self.button_layout[i].addWidget(self.camlabel[i])
+            self.button_layout[i].addWidget(self.camerasCombo[i])
 
         self.portlabel = QLabel("Порт: ")
-        self.portEdit = QLineEdit()
+        self.portEdit = [QLineEdit(), QLineEdit()]
         self.passwordlabel = QLabel("Пароль: ")
-        self.password_layout.addWidget(self.portlabel)
-        self.password_layout.addWidget(self.portEdit)
-        self.password_layout.addWidget(self.passwordlabel)
 
-        self.passwordEdit = QLineEdit()
-        self.passwordEdit.setEchoMode(QLineEdit.Password)
-        self.send = QPushButton('Ввести порт и пароль')
-        self.send.clicked.connect(self.sendPassword)
-        self.password_layout.addWidget(self.passwordEdit)
-        self.password_layout.addWidget(self.send)
+        for i in range(2):
+            self.password_layout[i].addWidget(self.portlabel)
+            self.password_layout[i].addWidget(self.portEdit[i])
+            self.password_layout[i].addWidget(self.passwordlabel)
 
-        self.layout.addLayout(self.button_layout)
-        self.layout.addLayout(self.password_layout)
+        self.passwordEdit = [QLineEdit(), QLineEdit()]
+        for i in range(2):
+            self.passwordEdit[i].setEchoMode(QLineEdit.Password)
+        self.send = [QPushButton('Ввести порт и пароль'), QPushButton('Ввести порт и пароль')]
+        self.send[0].clicked.connect(self.sendPassword0)
+        self.send[1].clicked.connect(self.sendPassword1)
 
-        self.streambutton = QPushButton("Начать трансляцию")
-        self.streambutton.clicked.connect(self.startStream)
-        self.layout.addWidget(self.streambutton)
-        self.streambutton.setDisabled(True)
+        for i in range(2):
+            self.password_layout[i].addWidget(self.passwordEdit[i])
+            self.password_layout[i].addWidget(self.send[i])
 
-        self.streamlink = QLabel()
-        self.streamlink.setText('')
-        self.streamlink.setOpenExternalLinks(True)
-        self.layout.addWidget(self.streamlink)
+            self.CameraLayout[i].addLayout(self.button_layout[i])
+            self.CameraLayout[i].addLayout(self.password_layout[i])
 
-        self.button2_layout = QHBoxLayout()
-        self.password2_layout = QHBoxLayout()
-        self.cam2label = QLabel('Камера 2: ')
-        self.cameras2Combo = QComboBox()
-        self.cameras2Combo.addItems(self.cameras)
-        self.cameras2Combo.activated[str].connect(self.onCameraSelect)
-        # self.cameras2Combo.activated[str].connect(self.start)
-        self.button2_layout.addWidget(self.cam2label)
-        self.button2_layout.addWidget(self.cameras2Combo)
+        self.streambutton = [QPushButton("Начать трансляцию"), QPushButton("Начать трансляцию")]
+        self.streambutton[0].clicked.connect(self.startStream0)
+        self.streambutton[1].clicked.connect(self.startStream1)
+        for i in range(2):
+            self.CameraLayout[i].addWidget(self.streambutton[i])
+            self.streambutton[i].setDisabled(True)
 
-        self.port2label = QLabel("Порт: ")
-        self.port2Edit = QLineEdit()
-        self.password2label = QLabel("Пароль: ")
-        self.password2_layout.addWidget(self.port2label)
-        self.password2_layout.addWidget(self.port2Edit)
-        self.password2_layout.addWidget(self.password2label)
-
-        self.password2Edit = QLineEdit()
-        self.password2Edit.setEchoMode(QLineEdit.Password)
-        self.send2 = QPushButton('Ввести порт и пароль')
-        # self.send2.clicked.connect(self.sendPassword)
-        self.password2_layout.addWidget(self.password2Edit)
-        self.password2_layout.addWidget(self.send2)
-
-        self.layout.addLayout(self.button2_layout)
-        self.layout.addLayout(self.password2_layout)
-
-        self.stream2button = QPushButton("Начать трансляцию")
-        # self.stream2button.clicked.connect(self.startStream)
-        self.layout.addWidget(self.stream2button)
-        self.stream2button.setDisabled(True)
-
-        self.stream2link = QLabel()
-        self.stream2link.setText('')
-        self.stream2link.setOpenExternalLinks(True)
-        self.layout.addWidget(self.stream2link)
+        self.streamlink = [QLabel(), QLabel()]
+        for i in range(2):
+            self.streamlink[i].setText('')
+            self.streamlink[i].setOpenExternalLinks(True)
+            self.CameraLayout[i].addWidget(self.streamlink[i])
 
 
         # Add a label
-        self.cameraview = QLabel()
-        self.layout.addWidget(self.cameraview)
+        self.cameraview = [QLabel(), QLabel()]
+        for i in range(2):
+            self.CameraLayout[i].addWidget(self.cameraview[i])
+            # Set the layout
+            self.layout.addLayout(self.CameraLayout[i])
 
-        # Set the layout
         self.setLayout(self.layout)
         self.setWindowTitle("NotVLC -- BETA ver. " + self.version)
-        self.setFixedSize(650, 640)
-        self.cameraindx = 0
-        self.camera = Camera(self.cameraindx)
-        self.cameraheight = 480
-        self.start()
+        # self.setFixedSize(650, 640)
+        self.cameraindx = [0, 1]
 
     def selectedCamera(self):
-        return self.cameraindx
+        return self.cameraindx[self.index]
 
     def start(self):
         if not self.camera.open():
@@ -126,11 +104,11 @@ class UI_Window(QWidget):
             msgBox = QMessageBox()
             msgBox.setText("Ошибка при открытии камеры.")
             msgBox.exec_()
-            self.streambutton.setDisabled(True)
+            self.streambutton[self.index].setDisabled(True)
             return
         else:
             if self.isPasswordEntered:
-                self.streambutton.setEnabled(True)
+                self.streambutton[self.index].setEnabled(True)
 
         self.timer.start(60)
 
@@ -139,41 +117,68 @@ class UI_Window(QWidget):
         if frame is not None:
             image = QImage(frame, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
             pixmap = QPixmap.fromImage(image)
-            self.cameraview.setPixmap(pixmap)
+            self.cameraview[self.index].setPixmap(pixmap)
 
     def onCameraSelect(self, text):
-        self.cameraindx = self.cameras.index(text)
-        self.camera = Camera(self.cameraindx)
+        self.cameraindx[self.index] = self.cameras.index(text)
+        if self.cameraindx[0] == self.cameraindx[1]:
+            print("Нельзя вести трансляцию с одной камеры в двух потоках")
+            return
+        self.camera = Camera(self.cameraindx[self.index])
         self.cameraheight = self.camera.getheight()
         if self.cameraheight is not None:
-            self.cameraview.setFixedSize(640, int(self.cameraheight))
-        # self.start()
+            self.cameraview[self.index].setFixedSize(640, int(self.cameraheight))
+        self.start()
+
+    def onCamera0Select(self, text):
+        self.index = 0
+        self.onCameraSelect(text)
+
+    def onCamera1Select(self, text):
+        self.index = 1
+        self.onCameraSelect(text)
 
     def startStream(self):
         self.timer.stop()
-        self.cameraview.clear()
-        self.cameraview.setFixedSize(640, int(self.cameraheight))
+        self.cameraview[self.index].clear()
+        self.cameraview[self.index].setFixedSize(640, int(self.cameraheight))
         self.password = self.sendPassword()
         webserver = WebServer(self.camera, int(self.port1), self.password)
-        selectedcamera = self.cameras[self.cameraindx]
+        selectedcamera = self.cameras[self.cameraindx[self.index]]
         streamadrr = webserver.getinfo()
         self.setWindowTitle(f"<<ИДЁТ ТРАНСЛЯЦИЯ>>")
         message = f'Идёт трансляция <i>{selectedcamera}</i> по адресу: <br />' \
                   f'<a href="{streamadrr}/admin:{self.password}">{streamadrr}/admin:{self.password}</a>'
-        self.streamlink.setText(message)
+        self.streamlink[self.index].setText(message)
         webserver.start()
-        self.camerasCombo.setDisabled(True)
-        self.streambutton.setDisabled(True)
+        self.camerasCombo[self.index].setDisabled(True)
+        self.streambutton[self.index].setDisabled(True)
+
+    def startStream0(self):
+        self.index = 0
+        self.startStream(self)
+
+    def startStream1(self):
+        self.index = 1
+        self.startStream(self)
 
     def sendPassword(self):
-        self.passwordText = self.passwordEdit.text()
-        self.port1 = self.portEdit.text()
+        self.passwordText = self.passwordEdit[self.index].text()
+        self.port1 = self.portEdit[self.index].text()
         if self.passwordText != '' and self.port1.isdigit() and 1 < int(self.port1) < 65536:
-            self.isPasswordEntered = True
-            self.streambutton.setEnabled(True)
+            self.isPasswordEntered[self.index] = True
+            self.streambutton[self.index].setEnabled(True)
         else:
-            self.isPasswordEntered = False
-            self.streambutton.setDisabled(True)
+            self.isPasswordEntered[self.index] = False
+            self.streambutton[self.index].setDisabled(True)
+
         return self.passwordText
 
+    def sendPassword0(self):
+        self.index = 0
+        self.sendPassword(self)
+
+    def sendPassword1(self):
+        self.index = 1
+        self.sendPassword(self)
 # TODO: add field to enter port
